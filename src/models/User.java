1d939af773f937502398a8fce6004b3176c3e9c3
Arunarivalagan743 <arunarivalagan774@gmail.com>
2025-12-26 19:45:15 +0530

    Add User model class

---
+++ src/models/User.java
@@ -0,0 +1,110 @@
+package models;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private String userId;
+    private String name;
+    private String email;
+    private String password;
+    private String role; // "USER" or "ADMIN"
+    private String area;
+    private String contact;
+    private LocalDateTime registeredDate;
+
+    // Constructor
+    public User(String userId, String name, String email, String password, String role) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.registeredDate = LocalDateTime.now();
+    }
+
+    // Getters and Setters
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public LocalDateTime getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(LocalDateTime registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+
+    public boolean isAdmin() {
+        return "ADMIN".equalsIgnoreCase(this.role);
+    }
+
+    public boolean isUser() {
+        return "USER".equalsIgnoreCase(this.role);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                ", area='" + area + '\'' +
+                ", contact='" + contact + '\'' +
+                ", registeredDate=" + registeredDate +
+                '}';
+    }
+}
