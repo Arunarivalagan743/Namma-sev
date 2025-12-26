09f2fddffd24d8d9ad63eef1647931a7aa18d3df
Arunarivalagan743 <arunarivalagan774@gmail.com>
2025-12-26 19:44:27 +0530

    Add Role enum for user roles

---
+++ src/enums/Role.java
@@ -0,0 +1,21 @@
+package enums;
+
+public enum Role {
+    USER("USER"),
+    ADMIN("ADMIN");
+
+    private final String roleName;
+
+    Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    @Override
+    public String toString() {
+        return roleName;
+    }
+}
